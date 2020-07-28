464ec760f9da80e149daf20a9ba663165f7dde14
santiagotabarez <1412tabarez@gmail.com>
2020-07-28 14:33:29 -0500

    semana 10

---
+++ healthyworld/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <table class="personal">
+      <tr>
+       <td>Nombres y apellidos </td>
+       <td>Edad </td>
+       <td>Correos electronicos</td>
+       <td>Celular  </td>
+   </tr>
+        
+        <tr>
+       <td>Juan David Casas </td>
+       <td>17 años</td>
+       <td>Juancasasa@iebarriosannicolas.edu.co</td>
+       <td>3108416603 </td>
+   </tr>
+   <tr>
+       <td>Alexander Maya </td>
+       <td>16 años</td>
+       <td>Edwinmayah@iebarriosannicolas.edu.co</td>
+       <td>3013059123</td>
+   </tr>
+   <tr>
+       <td>Santiago Tabarez </td>
+       <td>17 años</td>
+       <td>Santiagotabarezs@iebarriosannicolas.edu.co</td>
+       <td>3192707195</td>
+   </tr>
+        
+      </table>
+      
+    </div>
+  );
+}
+
+export default App;
